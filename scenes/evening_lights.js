7fd8524c746f61af2029aaa5cddc67954bc93a23
Théo Lepoutte <theolepoutte@gmail.com>
2021-12-31 12:59:04 +0100

    Added callbacks in chessboard.js and chess clock rotation

--- scenes/evening_lights.js
+++ scenes/evening_lights.js
@@ -1,4 +1,4 @@
-async function make_evening_lights_scene(gl, camera, physics_engine, light_set){
+async function make_evening_lights_scene(gl, camera, chessboard, physics_engine, light_set){
     // CUBEMAP
     const cubemap = await make_cubemap(gl, 'objects/misc/cube.obj', 'textures/cubemaps/Nalovardo', 1024);
 
@@ -17,23 +17,19 @@ async function make_evening_lights_scene(gl, camera, physics_engine, light_set){
     const table_mesh = load_obj('objects/misc/table.obj', true);
     const lamp_mesh = load_obj('objects/misc/lamp.obj', false);
     const lamp_table_mesh = load_obj('objects/misc/lamp_table.obj', true);
-    // const clk_mesh = load_obj('objects/misc/clock_main_lp.obj', false);
-    // const clk_min_mesh = load_obj('objects/misc/clock_min.obj', false);
-    // const clk_sec_mesh = load_obj('objects/misc/clock_sec.obj', false);
     const ch_clk_mesh = load_obj('objects/misc/ch_clock.obj', true);
     const ch_clk_hand_mesh = load_obj('objects/misc/ch_clock_hand.obj', false);
-
+    const ch_clk_sec_hand_mesh = load_obj('objects/misc/clock_sec.obj', false);
     // Make the buffer and the functions to draw the objects
     const glass_obj = await make_object(gl, glass_mesh);
     const table_obj = await make_object(gl, table_mesh, table_tex, table_bm);
     const lamp_obj = await make_object(gl, lamp_mesh);
     const lamp_table_obj = await make_object(gl, lamp_table_mesh, lamp_table_tex, lamp_table_bm);
-    // const clk_obj = await make_object(gl, clk_mesh);
-    // const clk_min_obj = await make_object(gl, clk_min_mesh);
-    // const clk_sec_obj = await make_object(gl, clk_sec_mesh);
     const ch_clk_obj = await make_object(gl, ch_clk_mesh, ch_clk_tex, null);
     const ch_clk_hand_l_obj = await make_object(gl, ch_clk_hand_mesh);
     const ch_clk_hand_r_obj = await make_object(gl, ch_clk_hand_mesh);
+    const ch_clk_sec_hand_l_obj = await make_object(gl, ch_clk_sec_hand_mesh);
+    const ch_clk_sec_hand_r_obj = await make_object(gl, ch_clk_sec_hand_mesh);
 
     // Positionning of the objects
     glass_obj.set_position(glMatrix.vec3.fromValues(-10.0, 1.0, 18.0));
@@ -48,12 +44,6 @@ async function make_evening_lights_scene(gl, camera, physics_engine, light_set){
 
     lamp_table_obj.set_position(glMatrix.vec3.fromValues(10.0, -5.0, 15.0));
 
-    // clk_obj.model = glMatrix.mat4.translate(clk_obj.model, clk_obj.model,
-    //     glMatrix.vec3.fromValues(0.0, -3.3, -12.0));
-    // clk_min_obj.model = glMatrix.mat4.translate(clk_min_obj.model, clk_min_obj.model,
-    //     glMatrix.vec3.fromValues(0.0, -3.3, -12.0));
-    // clk_sec_obj.model = glMatrix.mat4.translate(clk_sec_obj.model, clk_sec_obj.model,
-    //     glMatrix.vec3.fromValues(0.0, -3.3, -12.0));
     ch_clk_obj.set_position(glMatrix.vec3.fromValues(0.0, -5.0, -12.0));
     ch_clk_hand_l_obj.set_position(glMatrix.vec3.fromValues(-1.5, -3.3, -10.7));
     ch_clk_hand_l_obj.model = glMatrix.mat4.scale(ch_clk_hand_l_obj.model, ch_clk_hand_l_obj.model,
@@ -61,6 +51,12 @@ async function make_evening_lights_scene(gl, camera, physics_engine, light_set){
     ch_clk_hand_r_obj.set_position(glMatrix.vec3.fromValues(1.5, -3.3, -10.7));
     ch_clk_hand_r_obj.model = glMatrix.mat4.scale(ch_clk_hand_r_obj.model, ch_clk_hand_r_obj.model,
         glMatrix.vec3.fromValues(1.5, 1.5, 1.5));
+    ch_clk_sec_hand_l_obj.set_position(glMatrix.vec3.fromValues(-1.5, -3.3, -10.7));
+    ch_clk_sec_hand_l_obj.model = glMatrix.mat4.scale(ch_clk_sec_hand_l_obj.model, ch_clk_sec_hand_l_obj.model,
+        glMatrix.vec3.fromValues(0.7, 0.7, 0.7));
+    ch_clk_sec_hand_r_obj.set_position(glMatrix.vec3.fromValues(1.5, -3.3, -10.7));
+    ch_clk_sec_hand_r_obj.model = glMatrix.mat4.scale(ch_clk_sec_hand_r_obj.model, ch_clk_sec_hand_r_obj.model,
+        glMatrix.vec3.fromValues(0.7, 0.7, 0.7));
 
     // COLLISION BOXES
     const c_ch_clk_mesh = await load_obj('objects/collision_boxes/c_ch_clock.obj', false);
@@ -70,11 +66,11 @@ async function make_evening_lights_scene(gl, camera, physics_engine, light_set){
     // Point Lights
     const orange_pl = light_set.add_point_light(
         glMatrix.vec3.fromValues(0.0, 22.0, -50.0),
-        glMatrix.vec3.fromValues(5.0, 2.5, 0.0))
+        glMatrix.vec3.fromValues(5.0, 2.5, 0.0));
 
     const yellow_pl = light_set.add_point_light(
         glMatrix.vec3.fromValues(0.0, 20.0, 50.0),
-        glMatrix.vec3.fromValues(1.0, 1.0, 0.5))
+        glMatrix.vec3.fromValues(1.0, 1.0, 0.5));
     // Spotlights
     const lamp_sl = light_set.add_spot_light(
         glMatrix.vec3.fromValues(10.0, 7.0, 15.0),
@@ -82,7 +78,7 @@ async function make_evening_lights_scene(gl, camera, physics_engine, light_set){
         glMatrix.vec3.fromValues(0.0, -1.0, 0.0),
         20,
         180
-    )
+    );
 
     // CUSTOM CAMERA POSITIONNING
 
@@ -93,11 +89,23 @@ async function make_evening_lights_scene(gl, camera, physics_engine, light_set){
     physics_engine.register_object(ch_clk_obj, 0.0, c_ch_clk_mesh);
     physics_engine.register_object(lamp_table_obj, 0.0, c_lamp_table_mesh);
 
+    let _current_turn = chessboard.get_turn();
+
+    function set_current_turn(turn){
+        _current_turn = turn;
+    }
+
     function animate(time, delta_time){
         // Animate clock hands
-        // clk_min_obj.model = glMatrix.mat4.rotateZ(clk_min_obj.model, clk_min_obj.model, -6*delta_time / (60*1000*180) * Math.PI);
-        // clk_sec_obj.model = glMatrix.mat4.rotateZ(clk_sec_obj.model, clk_sec_obj.model, -6*delta_time / (1000*180) * Math.PI);
-        
+        if(_current_turn === 'w'){
+            ch_clk_hand_l_obj.model = glMatrix.mat4.rotateZ(ch_clk_hand_l_obj.model, ch_clk_hand_l_obj.model, 6*delta_time / (60*1000*180) * Math.PI);
+            ch_clk_sec_hand_l_obj.model = glMatrix.mat4.rotateZ(ch_clk_sec_hand_l_obj.model, ch_clk_sec_hand_l_obj.model, 6*delta_time / (1000*180) * Math.PI);
+        }
+        else{
+            ch_clk_hand_r_obj.model = glMatrix.mat4.rotateZ(ch_clk_hand_r_obj.model, ch_clk_hand_r_obj.model, 6*delta_time / (60*1000*180)  * Math.PI);
+            ch_clk_sec_hand_r_obj.model = glMatrix.mat4.rotateZ(ch_clk_sec_hand_r_obj.model, ch_clk_sec_hand_r_obj.model, 6*delta_time / (1000*180) * Math.PI);
+        }
+
         // Draw loop
         gl.clearColor(0.2, 0.2, 0.2, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -126,12 +134,11 @@ async function make_evening_lights_scene(gl, camera, physics_engine, light_set){
         table_obj.draw(shader);
         lamp_obj.draw(shader);
         lamp_table_obj.draw(shader);
-        // clk_obj.draw(shader);
-        // clk_min_obj.draw(shader);
-        // clk_sec_obj.draw(shader);
         ch_clk_obj.draw(shader);
         ch_clk_hand_l_obj.draw(shader);
         ch_clk_hand_r_obj.draw(shader);
+        ch_clk_sec_hand_l_obj.draw(shader);
+        ch_clk_sec_hand_r_obj.draw(shader);
     }
 
 
@@ -139,6 +146,7 @@ async function make_evening_lights_scene(gl, camera, physics_engine, light_set){
         animate:animate,
         draw_cubemap:draw_cubemap,
         draw:draw,
+        set_current_turn:set_current_turn,
     }
 }
 
--- scenes/scene.js
+++ scenes/scene.js
@@ -1,17 +1,17 @@
-async function make_scene(name, gl, camera, physics_engine, light_set) {
+async function make_scene(name, gl, camera, chessboard, physics_engine, light_set) {
 
     switch(name){
         case 'evening_lights':
-            return make_evening_lights_scene(gl, camera, physics_engine, light_set);
+            return make_evening_lights_scene(gl, camera, chessboard, physics_engine, light_set);
         case 'niagara_falls':
-            return make_niagara_falls_scene(gl, camera, physics_engine, light_set);
+            return make_niagara_falls_scene(gl, camera, chessboard, physics_engine, light_set);
         default:
-            return make_evening_lights_scene(gl, camera, physics_engine, light_set);
+            return make_evening_lights_scene(gl, camera, chessboard, physics_engine, light_set);
     }
 
 }
 
-async function make_niagara_falls_scene(gl, camera, physics_engine, light_set){
+async function make_niagara_falls_scene(gl, camer, chessboard, physics_engine, light_set){
 
 
     return{
