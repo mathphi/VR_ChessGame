003c1e9ffef918d9f2d97350d4dfdcf66ab8a0bc
Mathieu Philippart <philippartmathieu@gmail.com>
2022-01-02 18:59:16 +0100

    Added features (game undo, mouse button, shortcuts,...) + A lot of small fixes
     * Added undo feature by pressing CTRL+Z
     * Added mouse buttons inverting feature by pressing CTRL+M
     * Added keyboard shortcut for new game (CTRL+G)
     * Added reset function for scene objects
     * Added controls to avoid multiple animations on the same object at the same time
     * Changed the way objects are loaded (not async anymore)
     * Fixed camera center drift
     * Fixed time counter reset on new game
     * Fixed game over conditions

--- scenes/evening_lights.js
+++ scenes/evening_lights.js
@@ -15,40 +15,26 @@ async function make_evening_lights_scene(gl, camera, chessboard, physics_engine,
 
     // OBJECTS
     // Loading objects from files
-    const glass_mesh = load_obj('objects/misc/glass.obj', false);
-    const table_mesh = load_obj('objects/misc/table.obj', true);
-    const lamp_mesh = load_obj('objects/misc/lamp.obj', false, glMatrix.vec3.fromValues(20.0, 20.0, 20.0));
-    const lamp_table_mesh = load_obj('objects/misc/lamp_table.obj', true);
-    const ch_clk_mesh = load_obj('objects/misc/ch_clock.obj', true);
-    const ch_clk_hand_mesh = load_obj('objects/misc/ch_clock_hand.obj', false, glMatrix.vec3.fromValues(1.5, 1.5, 1.5));
-    const ch_clk_sec_hand_mesh = load_obj('objects/misc/clock_sec.obj', false, glMatrix.vec3.fromValues(0.7, 0.7, 0.7));
-    const plant_mesh = load_obj('objects/misc/plant.obj', true, glMatrix.vec3.fromValues(3.0, 3.0, 3.0));
+    const glass_mesh = await load_obj('objects/misc/glass.obj', false);
+    const table_mesh = await load_obj('objects/misc/table.obj', true);
+    const lamp_mesh = await load_obj('objects/misc/lamp.obj', false, glMatrix.vec3.fromValues(20.0, 20.0, 20.0));
+    const lamp_table_mesh = await load_obj('objects/misc/lamp_table.obj', true);
+    const ch_clk_mesh = await load_obj('objects/misc/ch_clock.obj', true);
+    const ch_clk_hand_mesh = await load_obj('objects/misc/ch_clock_hand.obj', false, glMatrix.vec3.fromValues(1.5, 1.5, 1.5));
+    const ch_clk_sec_hand_mesh = await load_obj('objects/misc/clock_sec.obj', false, glMatrix.vec3.fromValues(0.7, 0.7, 0.7));
+    const plant_mesh = await load_obj('objects/misc/plant.obj', true, glMatrix.vec3.fromValues(3.0, 3.0, 3.0));
 
     // Make the buffer and the functions to draw the objects
-    const glass_obj = await make_object(gl, glass_mesh);
-    const table_obj = await make_object(gl, table_mesh, table_tex, table_bm);
-    const lamp_obj = await make_object(gl, lamp_mesh);
-    const lamp_table_obj = await make_object(gl, lamp_table_mesh, lamp_table_tex, lamp_table_bm);
-    const ch_clk_obj = await make_object(gl, ch_clk_mesh, ch_clk_tex, null);
-    const ch_clk_hand_l_obj = await make_object(gl, ch_clk_hand_mesh);
-    const ch_clk_hand_r_obj = await make_object(gl, ch_clk_hand_mesh);
-    const ch_clk_sec_hand_l_obj = await make_object(gl, ch_clk_sec_hand_mesh);
-    const ch_clk_sec_hand_r_obj = await make_object(gl, ch_clk_sec_hand_mesh);
-    const plant_obj = await make_object(gl, plant_mesh, plant_tex, plant_bm);
-
-    // Positioning of the objects
-    glass_obj.set_position(glMatrix.vec3.fromValues(-10.0, 1.0, 18.0));
-    table_obj.set_position(glMatrix.vec3.fromValues(0.0, -5.0, 0.0));
-    lamp_obj.set_position(glMatrix.vec3.fromValues(0.0, -40.0, -50.0));
-    lamp_table_obj.set_position(glMatrix.vec3.fromValues(10.0, -5.0, 15.0));
-    ch_clk_obj.set_position(glMatrix.vec3.fromValues(0.0, -5.0, -12.0));
-    ch_clk_hand_l_obj.set_position(glMatrix.vec3.fromValues(-1.5, -3.3, -10.7));
-    ch_clk_hand_r_obj.set_position(glMatrix.vec3.fromValues(1.5, -3.3, -10.7));
-    ch_clk_sec_hand_l_obj.set_position(glMatrix.vec3.fromValues(-1.5, -3.3, -10.7));
-    ch_clk_sec_hand_r_obj.set_position(glMatrix.vec3.fromValues(1.5, -3.3, -10.7));
-    plant_obj.set_position(glMatrix.vec3.fromValues(10.0, -5.0, -20.0));
-
-    //TODO: reset clock hands position when starting a new game
+    const glass_obj = make_object(gl, glass_mesh);
+    const table_obj = make_object(gl, table_mesh, table_tex, table_bm);
+    const lamp_obj = make_object(gl, lamp_mesh);
+    const lamp_table_obj = make_object(gl, lamp_table_mesh, lamp_table_tex, lamp_table_bm);
+    const ch_clk_obj = make_object(gl, ch_clk_mesh, ch_clk_tex, null);
+    const ch_clk_hand_l_obj = make_object(gl, ch_clk_hand_mesh);
+    const ch_clk_hand_r_obj = make_object(gl, ch_clk_hand_mesh);
+    const ch_clk_sec_hand_l_obj = make_object(gl, ch_clk_sec_hand_mesh);
+    const ch_clk_sec_hand_r_obj = make_object(gl, ch_clk_sec_hand_mesh);
+    const plant_obj = make_object(gl, plant_mesh, plant_tex, plant_bm);
 
     // COLLISION BOXES
     const c_ch_clk_mesh = await load_obj('objects/collision_boxes/c_ch_clock.obj', false);
@@ -72,8 +58,6 @@ async function make_evening_lights_scene(gl, camera, chessboard, physics_engine,
         180
     );
 
-    // CUSTOM CAMERA POSITIONING
-
     // OBJECTS PHYSICS
     physics_engine.register_object(table_obj, 0.0);
     physics_engine.register_object(glass_obj, 1.0);
@@ -85,8 +69,56 @@ async function make_evening_lights_scene(gl, camera, chessboard, physics_engine,
     physics_engine.register_object(lamp_table_obj, 0.0, c_lamp_table_mesh);
     physics_engine.register_object(plant_obj, 0.0);
 
-    let _current_turn = chessboard.get_turn();
+    // Set initial object position, etc
+    reset(0.0);
 
+    function reset(anim_duration = 0.0) {
+        // Positioning of the objects
+        glass_obj.anim_to_position(glMatrix.vec3.fromValues(-10.0, 1.0, 18.0), anim_duration);
+        table_obj.anim_to_position(glMatrix.vec3.fromValues(0.0, -5.0, 0.0), anim_duration);
+        lamp_obj.anim_to_position(glMatrix.vec3.fromValues(0.0, -40.0, -50.0), anim_duration);
+        lamp_table_obj.anim_to_position(glMatrix.vec3.fromValues(10.0, -5.0, 15.0), anim_duration);
+        ch_clk_obj.anim_to_position(glMatrix.vec3.fromValues(0.0, -5.0, -12.0), anim_duration);
+        ch_clk_hand_l_obj.anim_to_position(glMatrix.vec3.fromValues(-1.5, -3.3, -10.7), anim_duration);
+        ch_clk_hand_r_obj.anim_to_position(glMatrix.vec3.fromValues(1.5, -3.3, -10.7), anim_duration);
+        ch_clk_sec_hand_l_obj.anim_to_position(glMatrix.vec3.fromValues(-1.5, -3.3, -10.7), anim_duration);
+        ch_clk_sec_hand_r_obj.anim_to_position(glMatrix.vec3.fromValues(1.5, -3.3, -10.7), anim_duration);
+        plant_obj.anim_to_position(glMatrix.vec3.fromValues(10.0, -5.0, -22.0), anim_duration);
+
+        // Positioning of the chessboard
+        const chessboard_rotation = glMatrix.quat.create();
+        glMatrix.quat.rotateY(chessboard_rotation, chessboard_rotation, Math.PI / 2.0);
+        chessboard.anim_to_position(glMatrix.vec3.fromValues(0.0, -5.0, 0.0), anim_duration, anim_duration === 0.0);
+        chessboard.anim_to_rotation(chessboard_rotation, anim_duration, anim_duration === 0.0);
+        chessboard.force_phycics(false);
+
+        const zero_rotation = glMatrix.quat.create();
+
+        // Reset orientation
+        glass_obj.anim_to_rotation(zero_rotation, anim_duration);
+        table_obj.anim_to_rotation(zero_rotation, anim_duration);
+        lamp_obj.anim_to_rotation(zero_rotation, anim_duration);
+        lamp_table_obj.anim_to_rotation(zero_rotation, anim_duration);
+        ch_clk_obj.anim_to_rotation(zero_rotation, anim_duration);
+        ch_clk_hand_l_obj.anim_to_rotation(zero_rotation, anim_duration);
+        ch_clk_hand_r_obj.anim_to_rotation(zero_rotation, anim_duration);
+        ch_clk_sec_hand_l_obj.anim_to_rotation(zero_rotation, anim_duration);
+        ch_clk_sec_hand_r_obj.anim_to_rotation(zero_rotation, anim_duration);
+        plant_obj.anim_to_rotation(zero_rotation, anim_duration);
+
+        // Reset physics mass
+        table_obj.set_physics_mass(0.0);
+        glass_obj.set_physics_mass(1.0);
+        ch_clk_obj.set_physics_mass(0.0);
+        ch_clk_hand_l_obj.set_physics_mass(0.0);
+        ch_clk_hand_r_obj.set_physics_mass(0.0);
+        ch_clk_sec_hand_l_obj.set_physics_mass(0.0);
+        ch_clk_sec_hand_r_obj.set_physics_mass(0.0);
+        lamp_table_obj.set_physics_mass(0.0);
+        plant_obj.set_physics_mass(0.0);
+    }
+
+    let _current_turn = chessboard.get_turn();
     function set_current_turn(turn){
         _current_turn = turn;
 
@@ -107,11 +139,16 @@ async function make_evening_lights_scene(gl, camera, chessboard, physics_engine,
         );
     }
 
+    let _game_stopped = false;
+    function on_game_event(event) {
+        _game_stopped = !!(event & ChessSituation.GameOver);
+    }
+
     const z_axis = glMatrix.vec3.fromValues(0.0, 0.0, 1.0);
 
     function animate(time, delta_time) {
         // Animate clock hands
-        if (_current_turn === 'w') {
+        if (_current_turn === 'w' && !_game_stopped) {
             ch_clk_hand_l_obj.rotate(z_axis, 6*delta_time / (60*1000*180) * Math.PI);
             ch_clk_sec_hand_l_obj.rotate(z_axis, 6*delta_time / (1000*180) * Math.PI);
         }
@@ -173,7 +210,9 @@ async function make_evening_lights_scene(gl, camera, chessboard, physics_engine,
         draw: draw,
         set_current_turn: set_current_turn,
         table: table_obj,
-        force_physics: force_physics
+        force_physics: force_physics,
+        reset: reset,
+        on_game_event: on_game_event
     }
 }
 
