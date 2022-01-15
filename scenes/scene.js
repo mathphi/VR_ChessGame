04ae9cc54b196f490034d18722e226eb45f6443c
Mathieu Philippart <philippartmathieu@gmail.com>
2022-01-15 22:59:06 +0100

    Added scene selection panel

--- scenes/scene.js
+++ scenes/scene.js
@@ -1,27 +1,70 @@
-async function make_scene(name, gl, camera, chessboard, physics_engine, light_set, particle_engine) {
 
-    switch(name){
-        case 'evening_lights':
-            return make_evening_lights_scene(gl, camera, chessboard, physics_engine, light_set, particle_engine);
-        case 'niagara_falls':
-            return make_niagara_falls_scene(gl, camera, chessboard, physics_engine, light_set, particle_engine);
-        case 'cosy_interior':
-            return make_cosy_interior_scene(gl, camera, chessboard, physics_engine, light_set, particle_engine);
-        default:
-            return make_evening_lights_scene(gl, camera, chessboard, physics_engine, light_set, particle_engine);
+function initialize_scene_selector(current_scene, on_scene_change) {
+    const scene_button = document.getElementById("scene");
+    const scenes_panel = document.getElementById("scenes-panel");
+
+    let panel_shown = false;
+    scene_button.addEventListener('click', function () {
+        if (panel_shown) {
+            panel_shown = false;
+            scene_button.classList.remove('active');
+            scenes_panel.classList.remove('shown');
+        }
+        else {
+            panel_shown = true;
+            scene_button.classList.add('active');
+            scenes_panel.classList.add('shown');
+        }
+    });
+
+    // Apply events to each item
+    for (const item of scenes_panel.children) {
+        // Only if it is a scene-item
+        if (item.classList.contains("scene-item")) {
+            item.addEventListener('click', function () {
+                panel_shown = false;
+                scene_button.classList.remove('active');
+                scenes_panel.classList.remove('shown');
+
+                if (item.classList.contains('selected'))
+                    return;
+
+                on_scene_change(item.getAttribute("value"));
+            });
+
+            if (item.getAttribute("value") === current_scene) {
+                item.classList.add("selected");
+            }
+        }
     }
-
 }
 
-async function make_niagara_falls_scene(gl, camer, chessboard, physics_engine, light_set, particle_engine){
+async function make_scene(name, gl, camera, chessboard, physics_engine, light_set, particle_engine) {
+    let scene_generator;
+
+    switch(name) {
+        case 'evening_lights':
+            scene_generator = make_evening_lights_scene;
+            break;
+        case 'niagara_falls':
+            scene_generator = make_niagara_falls_scene;
+            break;
+        case 'cosy_interior':
+            scene_generator = make_cosy_interior_scene;
+            break;
+        default:
+            scene_generator = make_evening_lights_scene;
+            break;
+    }
+
+    return scene_generator(gl, camera, chessboard, physics_engine, light_set, particle_engine);
+}
+
+async function make_niagara_falls_scene(gl, camera, chessboard, physics_engine, light_set, particle_engine) {
 
 
-    return{
-        draw: draw,
-        draw_cubemap: draw_cubemap,
-        animate: animate,
-        table: table,
-        force_physics: force_physics,
+    return {
+
     }
 }
 
